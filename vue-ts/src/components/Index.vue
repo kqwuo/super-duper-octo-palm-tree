b7a2ef656abc30e486d0d3f73687ff92abade94f
eltoredo <tanguy.e@hotmail.fr>
2021-09-18 22:31:53 +0200

    Made a table with hard data

---
+++ vue-ts/src/components/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+
+    <h1>{{ msg }}</h1>
+    <br>
+
+    <!--<h3>{{ info }}</h3>-->
+
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Destination</th>
+                <th>Departure</th>
+                <th>Reservation</th>
+                <th>Tickets number</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="flight in flights" :key="flight.id">
+                <td>{{flight.name}}</td>
+                <td>{{flight.destination}}</td>
+                <td>{{flight.departure}}</td>
+                <td><button type="button" @click="Book(flight.name)" class="btn btn-info">Book</button></td>
+                <td>{{flight.tickets}}</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div> 
+</template>
+
+<script>
+
+import { ref } from 'vue';
+//const axios = require('axios');
+
+export default {
+  name: "Index",
+  props: {
+    msg: String,
+  },
+  data () {
+    return {
+      //info: null
+    }
+  },
+  mounted () {
+    /*axios
+      .get('localhost:5000/api/flights')
+      .then(response => (this.info = response))*/
+  },
+  setup() {
+    const flights = ref([
+      { name: 'GOTA', destination: 'Aéroport Charles de Gaulle', departure: '20:00', tickets: 100 },
+      { name: 'NORA', destination: 'Aéroport de Londres', departure: '18:25', tickets: 100 },
+      { name: 'MONA', destination: 'Aéroport de Helsinki', departure: '14:30', tickets: 100 },
+      { name: 'TUPE', destination: 'Aéroport de Hong Hong', departure: '22:35', tickets: 100 },
+    ]);
+
+    return {
+      flights
+    };
+  },
+  methods: {
+    Book(name) {
+      // faire un post avec le nom de l'avion voulu
+      console.log(name);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
