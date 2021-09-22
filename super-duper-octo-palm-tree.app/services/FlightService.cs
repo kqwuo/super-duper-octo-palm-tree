89d69651e67840acdb05ae29aff926fe4d7f1e66
Dan Chiche <dan.chiche@signature-code.com>
2021-09-22 13:26:11 +0200

    Move SetOrder into OrderService

--- super-duper-octo-palm-tree.app/services/FlightService.cs
+++ super-duper-octo-palm-tree.app/services/FlightService.cs
@@ -18,52 +18,13 @@ namespace super_duper_octo_palm_tree.app.services
 
         public List<Flight> GetFlights()
         {
-
             return Mapper.GetFlights();
         }
 
-        //public Flight GetFlight(string idFlight)
-        //{
-        //    return _flightList.Find(flight => flight.IdFlight == idFlight);
-        //}
-
-        public bool SetOrder(Order order, string idFlight)
+        public Flight GetFlight(string idFlight)
         {
-            try
-            {
-                var flightToGet = GetFlights().Find(x => x.IdFlight == idFlight);
-
-                if (flightToGet == null)
-                    return false;
-
-                if (flightToGet.AvailableSeats > 0 && flightToGet.AvailableSeats >= order.NbBought)
-                {
-                    var idOrder = OrderDataRepository.CreateOrder(Mapper.OrderToOrderData(order), flightToGet.IdFlight);
-
-                    bool isFamily = (order.TicketList.Count >= 3
-                        && order.TicketList.Select(t => t.UserType == UserType.Adult).Count() >= 2
-                        && order.TicketList.Select(t => t.UserType == UserType.Child).Count() >= 1);
-
-                    foreach (Ticket ticket in order.TicketList)
-                    {
-                        ticket.BasePrice = flightToGet.BasePrice;
-                        ticket.AdditionalPrice = flightToGet.AdditionalLuggagePrice * ticket.NbAdditionalLuggage;
-                        ticket.BasePriceDiscount = (uint)(isFamily ? 10 : 0);
-                        TicketDataRepository.CreateTicket(Mapper.TicketToTicketData(ticket), idOrder);
-                    }
-
-                    flightToGet.AvailableSeats -= order.NbBought;
-
-                    FlightDataRepository.UpdateFlight(Mapper.FlightToFlightData(flightToGet));
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
+            return GetFlights().Find(flight => flight.IdFlight == idFlight);
         }
+
     }
 }
